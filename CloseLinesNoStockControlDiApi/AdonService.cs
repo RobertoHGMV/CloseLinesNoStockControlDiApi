40d5fe04f391abfd13101843dfa479818a9bc182
Roberto Gomes <roberto.vaz@inventsoftware.com.br>
2018-12-10 16:19:02 -0200

    Fechar linhas que não controlam estoque via diapi

---
+++ CloseLinesNoStockControlDiApi/AdonService.cs
@@ -0,0 +1,150 @@
+﻿using SAPbobsCOM;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace CloseLinesNoStockControlDiApi
+{
+    public class AdonService
+    {
+        public Company Company { get; private set; }
+
+        public string Server { get; set; }
+        public string CompanyDB { get; set; }
+        public string DbUserName { get; set; }
+        public string DbPassword { get; set; }
+        public BoDataServerTypes DbServerType { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public AdonService()
+        {
+            Company = new Company();
+        }
+
+        public void ConnectSbo()
+        {
+            Company.Server = Server;
+            Company.CompanyDB = CompanyDB;
+            Company.DbUserName = DbUserName;
+            Company.DbPassword = DbPassword;
+            Company.DbServerType = BoDataServerTypes.dst_MSSQL2014;
+            Company.UserName = UserName;
+            Company.Password = Password;
+
+            Company.UseTrusted = false;
+            Company.language = BoSuppLangs.ln_Portuguese_Br;
+            Company.XmlExportType = BoXmlExportTypes.xet_ExportImportMode;
+
+            if (Company.Connect() != 0)
+                throw new Exception("Erro ao conectar no sap:" +
+                           $"[{Company.GetLastErrorCode()}] - [{Company.GetLastErrorDescription()}]");
+        }
+
+        private string GetConnectionString()
+        {
+            var connStr = new SqlConnectionStringBuilder();
+            connStr.DataSource = Server;
+            connStr.InitialCatalog = CompanyDB;
+            connStr.UserID = DbUserName;
+            connStr.Password = DbPassword;
+            return connStr.ToString();
+        }
+
+        public void CloseLinesOfQuotation(int docEntry)
+        {
+            var businessObject = Company.GetBusinessObject(BoObjectTypes.oQuotations) as Documents;
+
+            if (!businessObject.GetByKey(docEntry))
+                throw new Exception($"Não foi possível localizar a cotação N°[{docEntry}] no SAP.");
+
+            var itensToCloseLines = GeItensWithNotStockControl(businessObject);
+            CloseLines(businessObject, itensToCloseLines);
+
+            if (businessObject.Update() != 0)
+                throw new Exception($"Erro ao atualizar documento no SAP.\n[{Company.GetLastErrorCode()}]-[{Company.GetLastErrorDescription()}]");
+        }
+
+        private void CloseLines(Documents businessObject, IList<string> itensToCloseLines)
+        {
+            for (var i = 0; i < businessObject.Lines.Count; i++)
+            {
+                businessObject.Lines.SetCurrentLine(i);
+
+                if (itensToCloseLines.Any(x => x == businessObject.Lines.ItemCode) &&
+                    businessObject.Lines.LineStatus == BoStatus.bost_Open)
+                    businessObject.Lines.LineStatus = BoStatus.bost_Close;
+            }
+        }
+
+        private IList<string> GeItensWithNotStockControl(Documents businessObject)
+        {
+            var itemsCode = new List<string>();
+
+            for (var i = 0; i < businessObject.Lines.Count; i++)
+            {
+                businessObject.Lines.SetCurrentLine(i);
+                itemsCode.Add(businessObject.Lines.ItemCode);
+            }
+
+            return GeItensWithNotStockControlFromDb(itemsCode);
+        }
+
+        private IList<string> GeItensWithNotStockControlFromDb(IList<string> listItemCode)
+        {
+            var itemsCode = new List<string>();
+            var data = GetItemCodeData(listItemCode);
+
+            foreach (DataRow row in data.Rows)
+            {
+                var itemCode = row.Field<string>("ItemCode");
+                itemsCode.Add(itemCode);
+            }
+
+            return itemsCode;
+        }
+
+        private DataTable GetItemCodeData(IList<string> listItemCode)
+        {
+            var connectionString = GetConnectionString();
+            var data = new DataTable();
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var command = conn.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = GetQuery(listItemCode);
+
+                conn.Open();
+                if (conn.State != ConnectionState.Open)
+                    throw new Exception("Não foi possível conectar ao banco de dados");
+
+                var reader = command.ExecuteReader();
+                data.Load(reader);
+                conn.Close();
+            }
+
+            return data;
+        }
+
+        private string GetQuery(IList<string> listItemCode)
+        {
+            var sb = new StringBuilder($@"SELECT [ItemCode] FROM [{CompanyDB}]..[OITM] 
+                                         WHERE [InvntItem] = 'N'
+                                         AND [ItemCode] IN(");
+
+            foreach (var itemCode in listItemCode)
+            {
+                if (itemCode == listItemCode.Last())
+                    sb.Append($"'{itemCode}');");
+                else
+                    sb.Append($"'{itemCode}',");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CloseLinesNoStockControlDiApi/Program.cs
@@ -0,0 +1,56 @@
+﻿using SAPbobsCOM;
+using System;
+
+namespace CloseLinesNoStockControlDiApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe o código da cotação para realizar o fechamento das linhas!");
+                var docEntry = Console.ReadLine();
+
+                var service = new AdonService();
+                SetParams(service);
+                service.ConnectSbo();
+                service.CloseLinesOfQuotation(Convert.ToInt32(docEntry));
+
+                PrintSuccess();
+            }
+            catch (Exception ex)
+            {
+                PrintMessageError(ex.Message);
+            }
+        }
+
+        private static void SetParams(AdonService service)
+        {
+            service.Server = @"sap91-pc\b1";
+            service.CompanyDB = "SBO_AREA_SEM_ADDON";
+            service.DbUserName = "sa";
+            service.DbPassword = "sap@123";
+            service.DbServerType = BoDataServerTypes.dst_MSSQL2014;
+            service.UserName = "manager";
+            service.Password = "sapbone";
+        }
+
+        private static void PrintSuccess()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("Operação realizada com sucesso");
+            Console.ReadKey();
+        }
+
+        private static void PrintMessageError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
